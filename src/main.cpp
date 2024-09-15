77456a0388995ca75a58a0f92d1f198e5cf02c01
Viktor Vorobev <vikeyer@gmail.com>
2024-09-15 15:29:54 +0200

    src: move stars that are to close back and render starting from behind
    This way we don't have the furthest stars render above the closest once.

--- src/main.cpp
+++ src/main.cpp
@@ -36,12 +36,21 @@ int main() {
 
   std::vector<Star> stars = createStars(conf::count);
 
+  uint32_t first = 0;
   while (window.isOpen()) {
     processEvents(window);
 
     // Fake travel toward increasing Z
-    for (auto &s : stars) {
+    for (uint32_t i{0}; i < conf::count; ++i) {
+      Star &s = stars[i];
       s.z -= conf::speed * conf::dt;
+      if (s.z < conf::near) {
+        // Offset the star by the excess travel it made behind near to keep the
+        // ordering of the stars
+        s.z = conf::far - (conf::near - s.z);
+        // This star is now the first we need to draw because it is further away
+        first = i;
+      }
     }
 
     // Rendering occurs here
@@ -50,19 +59,19 @@ int main() {
     sf::CircleShape shape{conf::radius};
     shape.setOrigin(conf::radius, conf::radius);
 
-    for (auto const &s : stars) {
-      if (s.z > conf::near) {
-        const float scale = 1.0f / s.z;
-        shape.setPosition(s.position * scale + conf::window_size_f * 0.5f);
-        shape.setScale(scale, scale);
+    for (uint32_t i{0}; i < conf::count; ++i) {
+      const uint32_t idx = (i + first) % conf::count;
+      const Star &s = stars[idx];
+      const float scale = 1.0f / s.z;
+      shape.setPosition(s.position * scale + conf::window_size_f * 0.5f);
+      shape.setScale(scale, scale);
 
-        const float depth_ratio = (s.z - conf::near) / (conf::far - conf::near);
-        const float color_ratio = 1.0f - depth_ratio;
-        const auto c = static_cast<uint8_t>(color_ratio * 255.0f);
-        shape.setFillColor({c, c, c});
+      const float depth_ratio = (s.z - conf::near) / (conf::far - conf::near);
+      const float color_ratio = 1.0f - depth_ratio;
+      const auto c = static_cast<uint8_t>(color_ratio * 255.0f);
+      shape.setFillColor({c, c, c});
 
-        window.draw(shape);
-      }
+      window.draw(shape);
     }
     window.display();
   }
